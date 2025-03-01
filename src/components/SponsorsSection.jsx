62fead53966e761cc63c0fad0bfec84b488a8c0a
neha199704 <neharawat0408@gmail.com>
2025-03-02 00:50:13 +0530

    optimise code

--- src/components/SponsorsSection.jsx
+++ src/components/SponsorsSection.jsx
@@ -5,16 +5,20 @@ import sponsor4 from "../assets/sponsor4.svg";
 import sponsor5 from "../assets/sponsor5.svg";
 import sponsor6 from "../assets/sponsor6.svg";
 
+const sponsors = [sponsor1, sponsor2, sponsor3, sponsor4, sponsor5, sponsor6];
+
 const Sponsors = () => {
   return (
-    <section className="flex justify-center   bg-[#D9D9D9]">
+    <section className="flex justify-center bg-[#D9D9D9]">
       <div className="flex flex-wrap justify-center sm:justify-between gap-3 md:gap-6 lg:gap-10">
-        <img className="w-32 md:w-40 lg:w-40" src={sponsor1} alt="Sponsor 1" />
-        <img className="w-32 md:w-40 lg:w-40" src={sponsor2} alt="Sponsor 2" />
-        <img className="w-32 md:w-40 lg:w-40" src={sponsor3} alt="Sponsor 3" />
-        <img className="w-32 md:w-40 lg:w-40" src={sponsor4} alt="Sponsor 4" />
-        <img className="w-32 md:w-40 lg:w-40" src={sponsor5} alt="Sponsor 5" />
-        <img className="w-32 md:w-40 lg:w-40" src={sponsor6} alt="Sponsor 6" />
+        {sponsors.map((sponsor, index) => (
+          <img
+            key={index}
+            src={sponsor}
+            alt={`Sponsor ${index + 1}`}
+            className="w-32 md:w-40 lg:w-40"
+          />
+        ))}
       </div>
     </section>
   );
